1ac710e2cc10f5e997e4e77f0a84dbdcdbe16dbc
Kajihara <k.taisei.2000@gmail.com>
2022-03-30 11:42:42 +0900

    データを眺めた

--- learning/nn.py
+++ learning/nn.py
@@ -1,1 +1,39 @@
-#jjj
+import numpy as np
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+# defining dir variables 
+root_dir = 'C:/Users/ktais/Documents/HaitLab/'
+train_dir = 'C:/Users/ktais/Documents/HaitLab/train/'
+test_dir = 'C:/Users/ktais/Documents/HaitLab/test/'
+
+target_var = os.listdir(train_dir)
+# print(target_var)
+
+import matplotlib.pyplot as plt
+
+fig, axes = plt.subplots(1, 7, figsize=(20,8))
+for i in range(len(target_var)):
+    d = train_dir+target_var[i]+'/'+os.listdir(train_dir+target_var[i]+'/')[0]
+    axes[i].imshow( plt.imread(d) )
+    axes[i].set_title(target_var[i])
+plt.show()
+
+x_train = np.array([ len(os.listdir(train_dir+i+'/')) for i in target_var ])
+x_test = np.array([ len(os.listdir(test_dir+i+'/')) for i in target_var ])
+label = target_var
+
+fig, axes = plt.subplots(1, 2, figsize=(8,4))
+axes[0].pie(x_train, labels=label, autopct='%1.1f%%',shadow=True, startangle=90)
+axes[1].pie(x_test, labels=label, autopct='%1.1f%%',shadow=True, startangle=90)
+axes[0].set_title('Train')
+axes[1].set_title('Test')
+plt.show()
+
+for i in target_var:
+    print('Emotion : ' + i )
+    print('\tTraining : ' + str(len(os.listdir(train_dir+i+'/'))) +'\n\t Testing : ' + str(len(os.listdir(test_dir+i+'/'))))
+
+
