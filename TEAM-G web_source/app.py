271926bc1c6dce24c60e9e17486f85437b9daf52
daisuke <dai.special2796@gmail.com>
2022-03-31 18:31:28 +0900

    so far

--- TEAM-G web_source/app.py
+++ TEAM-G web_source/app.py
@@ -2,8 +2,18 @@ from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def result():
-    return render_template('result.html', emotion="不快")
-if __name__ == '__main__':
+@app.route("/")
+def index_form():
+    return render_template("index.html")
+
+@app.route("/result", methods=["GET", "POST"])
+def request_form():
+    if request.method == "POST":
+        print(type(request.files["img"]))
+        return render_template("result.html", emotion="不快")
+    else:
+        return render_template("result.html", emotion="不快")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
